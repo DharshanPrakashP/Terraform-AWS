84834c4fb951dc232336c01613cc990bf1fb1da8
DharshanPrakash <dharshanprakasherode@gmail.com>
2024-05-03 19:02:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_lambda_function" "on_boarding_lambda" {
   # s3_bucket = var.config_bucket
   # s3_key    = var.on_boarding_lambda_config_path
   filename      = "python/lambda_function.zip"
-  function_name = "Test lambda"
+  function_name = "Test-lambda"
   runtime       = "python3.8"
   handler       = "lambda_function.lambda_handler"
   # layers        = [aws_lambda_layer_version.lambda_layer.arn, aws_lambda_layer_version.lambda_layer_2.arn]

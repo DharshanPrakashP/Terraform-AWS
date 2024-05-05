8fa1a0505de8c0b9933d2eb853cc08549bceb0f0
swathikuppusamy <swathikuppusamy2005@gmail.com>
2024-05-05 12:27:15 +0530

    commit1

--- main.tf
+++ main.tf
@@ -1,1 +1,34 @@
+provider "aws" {
+  region = "ap-south-1"
 
+  
+}
+resource "aws_iam_role" "test-lambda" {
+  name = "test-lambda"
+  assume_role_policy = <<EOF
+ {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "lambda.amazonaws.com"
+        ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+  
+}
+
+resource "aws_lambda_function" "test-lambda" {
+  filename = "python/lambda_function.zip"
+  role = aws_iam_role.test-lambda.arn
+  function_name = "test-lambda"
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.8"
+}

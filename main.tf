6ca6dd4443db21ca9efbd546eb1fb2d66f6f03dc
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-05-07 19:32:16 +0530

    commit

--- main.tf
+++ main.tf
@@ -25,11 +25,13 @@ resource "aws_security_group" "security_kaaraalan" {
     Name = "security_kaaraalan"
   }
   ingress {
-    
+    from_port = 0
+    to_port = 0
     protocol = "-1"
   }
   egress {
-    
+    from_port = 0
+    to_port = 0
     protocol = "-1"
     
   

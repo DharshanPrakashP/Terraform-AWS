3464d922ea3846822d1168aa877074c5f9ccf594
sowbaraniga <sowbaranigakps04@gmail.com>
2024-05-07 20:09:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,12 +37,15 @@ resource "aws_vpc" "vpctest" {
 resource "aws_security_group" "test_sec" {
   vpc_id = aws_vpc.vpctest.id
   ingress = {
+    from_port = 0
+    to_port = 0
     
     protocol = "-1"
     cidr_block = ["0.0.0.0/0"]
   }
   egress = {
-   
+    from_port = 0
+    to_port = 0
     protocol = "-1"
     cidr_block = ["0.0.0.0/0"]
   }
@@ -57,6 +60,6 @@ resource "aws_instance" "instest" {
   ami = "ami-07caf09b362be10b8"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.test_subnet.id
-  vpc_security_group_ids = [aws_security_group.test_sec.id]
+ # vpc_security_group_ids = [aws_security_group.test_sec.id]
 }
   

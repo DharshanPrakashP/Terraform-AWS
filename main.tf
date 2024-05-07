a3590943145f6eb161b2d370e50021afa98eeb08
sowbaraniga <sowbaranigakps04@gmail.com>
2024-05-07 20:02:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,99 +1,62 @@
 provider "aws" {
   region = "ap-south-1"
-
 }
-
-# resource "aws_vpc" "Test" {
-#   cidr_block = "10.0.0.0/16"
-
+# resource "aws_iam_role" "lambda-test" {
+#   name = "lambda-test"
+#   assume_role_policy = <<EOF
+#  {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "",
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": [
+#           "lambda.amazonaws.com"
+#         ]
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# EOF
+  
 # }
 
-# resource "aws_security_group" "Test-secgrp" {
-
-#   vpc_id = aws_vpc.Test.id
-
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
+# resource "aws_lambda_function" "lambda-test" {
+#   filename = "python/lambda_function.zip"
+#   role = aws_iam_role.lambda-test.arn
+#   function_name = "lambda-test"
+#   handler = "lambda_function.lambda_handler"
+#   runtime = "python3.8"
 # }
-
-# resource "aws_subnet" "Test-sbnet" {
-
-#   vpc_id            = aws_vpc.Test.id
-#   cidr_block        = "10.0.0.0/24"
-#   availability_zone = "ap-south-1a"
-
-# }
-
-# resource "aws_instance" "Test" {
-#   ami                    = "ami-013e83f579886baeb"
-#   instance_type          = "t2.micro"
-#   subnet_id              = aws_subnet.Test-sbnet.id
-#   vpc_security_group_ids = [aws_security_group.Test-secgrp.id]
-# }
-
-resource "aws_iam_role" "ttt" {
-  name                = "test-role"
-  assume_role_policy  = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-          "lambda.amazonaws.com"
-        ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+resource "aws_vpc" "vpctest" {
+  cidr_block = "10.0.0.0/16"
+  
 }
-EOF
-
+resource "aws_security_group" "test_sec" {
+  vpc_id = aws_vpc.vpctest.id
+  ingress = {
+    
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+  }
+  egress = {
+   
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+  }
 }
-
-resource "aws_iam_role_policy" "ttt" {
-  name = "test-role-policy"
-  role = aws_iam_role.ttt.id
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*"
-    }
-  ]
+resource "aws_subnet" "test_subnet" {
+  vpc_id = aws_vpc.vpctest.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  
 }
-EOF
+resource "aws_instance" "instest" {
+  ami = "ami-07caf09b362be10b8"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.test_subnet.id
+  vpc_security_group_ids = [aws_security_group.test_sec.id]
 }
-
-resource "aws_lambda_function" "on_boarding_lambda" {
-  # s3_bucket = var.config_bucket
-  # s3_key    = var.on_boarding_lambda_config_path
-  filename      = "python/lambda_function.zip"
-  function_name = "Test-lambda"
-  runtime       = "python3.8"
-  handler       = "lambda_function.lambda_handler"
-  # layers        = [aws_lambda_layer_version.lambda_layer.arn, aws_lambda_layer_version.lambda_layer_2.arn]
-  role = aws_iam_role.ttt.arn
-}
-
+  

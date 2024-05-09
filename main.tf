54ab52e4056a3aeafc4d806950e6258549c3e787
DharshanPrakash <dharshanprakasherode@gmail.com>
2024-05-09 19:01:05 +0530

    subfile changes

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "tf-rp-states"
-    key            = "terraform.tfstate"
+    key            = "jayasurya/terraform.tfstate"
     region         = "us-east-1"
     # dynamodb_table = "terraform_locks"
   }
